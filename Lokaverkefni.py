a4d5adb48f2d29228aa2a18bd74ffaf6d397c8fc
PeturSteinn <peturgu220@nemi.tskoli.is>
2017-11-28 18:32:55 +0000

    Leikur að mestu leyti tilbúinn

--- Lokaverkefni.py
+++ Lokaverkefni.py
@@ -14,18 +14,48 @@ class nagdyr:
 def action(nagdyr):
     global switchDirection
     switchDirection = False
+
+    def checkIfCombat():
+        if abs(mus.stad - rotta1.stad) <= 3: # Ef hún er nálægt rotta1
+            print(mus.nafn + " er nálægt " + rotta1.nafn)
+            if mus.afl < rotta1.afl:
+                print(rotta1.nafn + " hendir músinni um " + str(rotta1.afl) + " reiti")
+                mus.stad -= rotta1.afl
+            elif mus.afl > rotta1.afl:
+                print(mus.nafn + " sigrar " + rotta1.nafn + " því hún er með meira afl")
+
+            else:
+                print("Jafntefli á milli " + mus.nafn + " og " + rotta1.nafn)
+
+        elif abs(mus.stad - rotta2.stad) <= 3:  # Ef hún er nálægt rotta2
+            print(mus.nafn + " er nálægt " + rotta2.nafn)
+            if mus.afl < rotta2.afl:
+                print(rotta2.nafn + " hendir músinni um " + str(rotta2.afl) + " reiti")
+                mus.stad -= rotta2.afl
+
+            elif mus.afl > rotta2.afl:
+                print(mus.nafn + " sigrar " + rotta2.nafn + " því hún er með meira afl")
+
+            else:
+                print("Jafntefli á milli " + mus.nafn + " og " + rotta2.nafn)
+        elif abs(mus.stad - rotta3.stad) <= 3:   # Ef hún er nálægt rottu3
+            print(mus.nafn + " er nálægt " + rotta3.nafn)
+            if mus.afl < rotta3.afl:
+                print(rotta3.nafn + " hendir músinni um " + str(rotta3.afl) + " reiti")
+                mus.stad -= rotta3.afl
+
+            elif mus.afl > rotta3.afl:
+                print(mus.nafn + " sigrar " + rotta3.nafn + " því hún er með meira afl")
+
+            else:
+                print("Jafntefli á milli " + mus.nafn + " og " + rotta3.nafn)
     kast = random.randint(1, 6)
     if (nagdyr.teg == "mús"):
         for i in range(kast):
             print("Mús færir sig frá reit", nagdyr.stad, "yfir á reit ", end='')
             mus.stad += 1
             print(nagdyr.stad)
-            if abs(nagdyr.stad - rotta1.stad) <= 3:
-                print(nagdyr.nafn + " er nálægt " + rotta1.nafn)
-            elif abs(nagdyr.stad - rotta2.stad) <= 3:
-                print(nagdyr.nafn + " er nálægt " + rotta2.nafn)
-            elif abs(nagdyr.stad - rotta3.stad) <= 3:
-                print(nagdyr.nafn + " er nálægt " + rotta3.nafn)
+            checkIfCombat()
 
             if nagdyr.stad == hamstur.stad:
                 print("Hamstur kastar mús um", hamstur.afl, "reiti")
@@ -44,40 +74,27 @@ def action(nagdyr):
                     print(nagdyr.nafn + " færir sig frá reit", nagdyr.stad, "yfir á reit ", end='')
                     nagdyr.stad += 1
                     print(nagdyr.stad)
-                    print(nagdyr.nafn + " Switched direction -----------------------")
-                    if abs(nagdyr.stad - mus.stad) <= 3:
-                        print(nagdyr.nafn + " er nálægt músinni!")
-
                     switchDirection = True
 
                 else:
                     print(nagdyr.nafn + " færir sig frá reit", nagdyr.stad, "yfir á reit ", end='')
                     nagdyr.stad -= 1
                     print(nagdyr.stad)
-                    if abs(nagdyr.stad - mus.stad) <= 3:
-                        print(nagdyr.nafn + " er nálægt músinni!")
-
-
-
-
+                checkIfCombat()
 
         elif fiddy == 1:
             print(nagdyr.nafn + " fer til hægri um " + str(abs(kast)) + " reiti og er staðsett núna á " + str(nagdyr.stad))
             for i in range(kast):
                 if nagdyr.stad >= 100 or switchDirection:
                     nagdyr.stad -= 1
-                    print(nagdyr.nafn + " Switched direction -----------------------")
-                    if abs(nagdyr.stad - mus.stad) <= 3:
-                        print(nagdyr.nafn + " er nálægt músinni!")
-
                     switchDirection = True
 
                 else:
                     print(nagdyr.nafn + " færir sig frá reit", nagdyr.stad, "yfir á reit ", end='')
                     nagdyr.stad += 1
                     print(nagdyr.stad)
-                    if abs(nagdyr.stad - mus.stad) <= 3:
-                        print(nagdyr.nafn + " er nálægt músinni!")
+                checkIfCombat()
+
 
 
     elif (nagdyr.teg == "hamstur"):
--- README.md
+++ README.md
@@ -1,8 +1,12 @@
 # FORR2-Lokaverkefni
 
 ## Dagbók
-23.11.17
-Gerðum classan til að gefa þeim stats
-28.11.17
-Pushuðum Python Skjalið
-Bættum slatta í python skjalinu
+#####**23.11.17:**
+Gerðum classan til að gefa þeim stats.
+#####**28.11.17 í tímanum:**
+Bættum við fullt af breytum sem verða notaðar síðar, gerðum nýjann  
+klassa sem hét labb() en núna heitir hann action() sem er kjarni forritsins.
+#####**28.11.17 kl 16:**
+Kláruðum virknina fyrir nagdýrin fyrir utan combat systemið.  
+Ásamt því var bætt við fullt af print() til að fylgjast með öllu sem gerist í hverri keyrslu.
+## Helgi Tuan og Pétur Steinn
